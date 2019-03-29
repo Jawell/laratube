96857b54dd088e739a0990319a9f10da99e2c571
jawell <skytor08@gmail.com>
2019-03-29 07:52:37 +0200

    move categories to single component

---
+++ resources/js/components/Helpers/Categories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Categories extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+                <option value="2">Cars & Vehicles</option>
+                <option value="23">Comedy</option>
+                <option value="27">Education</option>
+                <option value="24">Entertainment</option>
+                <option value="1">Film & Animation</option>
+                <option value="20">Gaming</option>
+                <option value="26">How-to & Style</option>
+                <option value="10">Music</option>
+                <option value="25">News & Politics</option>
+                <option value="29">Non-profits & Activism</option>
+                <option value="22">People & Blogs</option>
+                <option value="15">Pets & Animals</option>
+                <option value="28">Science & Technology</option>
+                <option value="17">Sport</option>
+                <option value="19">Travel & Events</option>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Categories;
